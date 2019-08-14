6445e0bad214ac7a3ef7e96e1205273cc86b529a
bchutchison <benhutchison85@gmail.com>
2019-08-14 11:37:26 +0100

    Added post form

---
+++ client/src/App.vue
@@ -0,0 +1,38 @@
+<template lang="html">
+  <div id="app">
+    <bookings-form />
+    <!-- <bookings-grid :bookings="bookings" /> -->
+  </div>
+</template>
+
+<script>
+import BookingsForm from './components/BookingsForm';
+import BookingService from './services/BookingService.js';
+
+export default {
+  name: 'app',
+  components: {
+    'bookings-form': BookingsForm
+  },
+  data ()  {
+    return {
+      bookings: []
+    }
+  },
+
+  mounted(){
+    this.fetchData();
+
+  },
+
+  methods: {
+    fetchData(){
+      BookingService.getBookings()
+      .then(bookings => this.bookings = bookings);
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ client/src/components/BookingsForm.vue
@@ -0,0 +1,46 @@
+<template lang="html">
+  <form @submit="handleSubmit" id="bookings-form">
+    <div class="wrapper">
+      <label for="name">Name</label>
+      <input v-model="name" id="name" type="text">
+    </div>
+    <div class="wrapper">
+      <label for="email">Email</label>
+      <input v-model="email" id="email" type="text">
+    </div>
+    <div class="wrapper">
+      <label for="checked-in">Checked In</label>
+      <input v-model="checkedIn" type="checkbox" name="checked-in">
+    </div>
+    <input type="submit" value="Add Booking">
+  </form>
+</template>
+
+<script>
+import BookingService from '../services/BookingService.js';
+
+export default {
+  name: 'bookings-form',
+  data() {
+    return {
+      name: '',
+      email: '',
+      checkedIn: false
+    }
+  },
+  methods: {
+    handleSubmit(evt) {
+      evt.preventDefault();
+      const payload = {
+        name: this.name,
+        email: this.email,
+        checkedIn: this.checkedIn
+      };
+      BookingService.postBooking(payload);
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
